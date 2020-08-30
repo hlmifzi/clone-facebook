5cf6dfd71146f93fa565026216bf41d2b56b0afb
Helmi <hlmifzi@gmail.com>
2020-08-30 12:11:42 +0700

    solve spec reinspect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import PatchedReactDOM from '@hot-loader/react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+if (process.env.REACT_DOM === 'patched') {
+  PatchedReactDOM.render(<App />, document.getElementById('root'));
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
